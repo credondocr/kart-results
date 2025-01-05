eb5baf7376a0d9a27f8fd9d022992218b6ba8a0c
Cesar Redondo <credondocr@gmail.com>
2025-01-05 01:45:21 -0600

    split data

--- src/data/history/2024/invierno/starts/index.ts
+++ src/data/history/2024/invierno/starts/index.ts
@@ -1,7 +1,7 @@
 import { Class } from "@/data/types";
 
 export const stars : Class = {
-  title: 'TILLOTSON',
+  title: 'STARS OF TOMORROW',
   ageGroup: 'Age 8-12',
   details: [
     '8-12 years old',
@@ -12,267 +12,178 @@ export const stars : Class = {
   img: 'https://kartshop.com/images/VLR1-2-p.webp',
   categories: [
     {
-      name: 'TILLOTSON - HEAVY',
-      results: [
-        {
-          number: 969,
-          country: 'CR',
-          driver: 'Cesar Redondo',
-          team: 'fsa',
-          scores: [ 0, 0, 0, 11, 24.5, 24.5 ],
-          worst: 0
-        },
-        {
-          number: 924,
-          country: 'CR',
-          driver: 'Henry Nanne',
-          team: 'valerio',
-          scores: [
-            0,
-            42.5,
-            42.5,
-            35,
-            42.5,
-            42.5
-          ],
-          worst: 35
-        },
-        {
-          number: 912,
-          country: 'CR',
-          driver: 'Nicolas Otero',
-          team: 'Advanced',
-          scores: [ 0, 28, 31, 0, 0, 0 ],
-          worst: 0
-        },
-        {
-          number: 977,
-          country: 'CR',
-          driver: 'Javier Jose Mora',
-          team: 'mhkarting',
-          scores: [ 0, 30, 30, 41.5, 32, 11 ],
-          worst: 30
-        },
-        {
-          number: 916,
-          country: 'CR',
-          driver: 'Erick Mauricio Brenes',
-          team: 'advanced',
-          scores: [ 19.5, 0, 0, 0, 5, 21 ],
-          worst: 0
-        }
-      ]
-    },
-    {
-      name: 'TILLOTSON - JUNIOR',
-      results: [
-        {
-          number: 956,
-          country: 'CR',
-          driver: 'Jian Luca Pastore',
-          team: 'mhkarting',
-          scores: [
-            32.5,
-            37.5,
-            42.5,
-            24,
-            11,
-            42.5
-          ],
-          worst: 35
-        },
-        {
-          number: 945,
-          country: 'CR',
-          driver: 'Federico Rojas',
-          team: 'Advanced',
-          scores: [ 28, 35, 31, 40.5, 0, 0 ],
-          worst: 0
-        },
-        {
-          number: 922,
-          country: 'CR',
-          driver: 'Tomas Arias',
-          team: 'valerio',
-          scores: [ 0, 30, 24, 30, 35, 33 ],
-          worst: 24
-        },
-        {
-          rank: 2,
-          number: 998,
-          country: 'CR',
-          driver: 'Santiago Navas',
-          team: 'advanced',
-          scores: [ 28, 35, 31, 40.5, 0, 0 ],
-          worst: 42
-        },
-        {
-          rank: 2,
-          number: 984,
-          country: 'CR',
-          driver: 'Julian Serrano',
-          team: 'Advanced',
-          scores: [
-            33.5,
-            18.5,
-            19.5,
-            25,
-            24.5,
-            19.5
-          ],
-          worst: 38
-        },
-        {
-          rank: 2,
-          number: 908,
-          country: 'CR',
-          driver: 'Mayra Londoño',
-          team: 'Advanced',
-          scores: [ 0, 16, 17, 17.5, 19.5, 16 ],
-          worst: 16
-        },
-        {
-          rank: 2,
-          number: 928,
-          country: 'CR',
-          driver: 'Emil Brautigam ',
-          team: 'formula',
-          scores: [ 19.5, 0, 0, 0, 5, 21 ],
-          worst: 0
-        }
-      ]
-    },
-    {
-      name: 'TILLOTSON - SENIOR',
+      name: '',
       results: [
         {
           rank: 1,
-          number: 905,
-          driver: 'Rafael Fabregas',
-          scores: [ 0, 41.5, 42.5, 35, 34, 39 ],
-          worst: 34,
-          country: 'PA',
-          team: 'fmv'
+          number: 155,
+          driver: 'Carlos Andres Valverde',
+          team: 'fik',
+          country: 'CR',
+          scores: [
+            42.5,
+            41.5,
+            41.5,
+            37.5,
+            26,
+            21.5
+          ],
+          points: 210.5,
+          worst: 47.5
         },
         {
           rank: 2,
-          number: 927,
-          driver: 'Eduardo Glenn',
-          scores: [
-            39,
-            33,
-            21,
-            23.5,
-            42.5,
-            17.5
-          ],
-          worst: 44.5,
+          number: 105,
+          driver: 'Emilio Bolaños',
+          team: 'fik',
           country: 'CR',
-          team: 'Advanced'
+          scores: [ 0, 5, 26, 0, 42.5, 42.5 ],
+          points: 116,
+          worst: 0
         },
         {
           rank: 3,
-          number: 920,
-          driver: 'Mathias Hernandez',
-          scores: [ 33.5, 20.5, 29, 22.5, 7, 0 ],
-          worst: 7,
+          number: 141,
+          driver: 'Jaden Spielman',
+          team: 'fik',
           country: 'CR',
-          team: 'formula'
+          scores: [ 0, 7, 12.5, 30.5, 22, 31 ],
+          points: 103,
+          worst: 12.5
         },
         {
           rank: 4,
-          number: 971,
-          driver: 'Andres Valldeperas',
-          scores: [ 19.5, 20, 16, 40.5, 0, 0 ],
-          worst: 0,
+          number: 152,
+          driver: 'Daniela Dorado',
+          team: 'fik',
           country: 'CR',
-          team: 'Advanced'
+          scores: [ 15.5, 8, 18.5, 22, 30, 30 ],
+          points: 124,
+          worst: 34
         },
         {
           rank: 5,
-          number: 906,
-          driver: 'Natalia Bräutigam',
-          scores: [
-            15.5,
-            26.5,
-            22.5,
-            20,
-            22,
-            11
-          ],
-          worst: 35.5,
+          number: 124,
+          driver: 'Julian Lahmann',
+          team: 'fik',
           country: 'CR',
-          team: 'Advanced'
+          scores: [ 0, 24.5, 33, 29, 10.5, 0 ],
+          points: 97,
+          worst: 10.5
         },
         {
           rank: 6,
-          number: 957,
-          driver: 'Osvaldo Blanco',
-          scores: [
-            14.5,
-            9,
-            19.5,
-            15,
-            9.5,
-            28.5
-          ],
-          worst: 23.5,
+          number: 119,
+          driver: 'Matthew Phillips',
+          team: 'mhkarting',
           country: 'CR',
-          team: 'fmv'
+          scores: [ 28, 27, 14, 13.5, 10.5, 15 ],
+          points: 108,
+          worst: 24
         },
         {
           rank: 7,
-          number: 901,
-          driver: 'Daniel Sequeira',
-          scores: [ 17.5, 14, 15.5, 0, 15, 22.5 ],
-          worst: 14,
+          number: 127,
+          driver: 'Isabella Hernandez',
+          team: 'advanced',
           country: 'CR',
-          team: 'losprimos'
+          scores: [
+            19.5,
+            12.5,
+            9.5,
+            19.5,
+            14.5,
+            16.5
+          ],
+          points: 92,
+          worst: 22
         },
         {
           rank: 8,
-          number: 910,
-          driver: 'Stefano Morice',
-          scores: [ 13, 16.5, 20, 15.5, 0, 0 ],
-          worst: 0,
+          number: 198,
+          driver: 'Theo Grunwedl',
+          team: 'advanced',
           country: 'CR',
-          team: 'fsa'
+          scores: [
+            13,
+            22,
+            18,
+            14.5,
+            14.5,
+            12.5
+          ],
+          points: 94.5,
+          worst: 25.5
         },
         {
           rank: 9,
-          number: 929,
-          driver: 'Luca Albanese',
-          scores: [ 11, 0, 0, 13, 16.5, 6 ],
-          worst: 0,
+          number: 184,
+          driver: 'Montserrat Monge',
+          team: 'Advanced',
           country: 'CR',
-          team: 'mhkarting'
+          scores: [ 14, 16, 19.5, 13, 12, 10.5 ],
+          points: 85,
+          worst: 22.5
         },
         {
           rank: 10,
-          number: 921,
-          driver: 'Manrique Valverde',
-          scores: [ 31, 0, 0, 0, 0, 0 ],
-          worst: 0,
+          number: 107,
+          driver: 'Marcelo Piñago',
+          team: 'valerio',
           country: 'CR',
-          team: 'fmv'
+          scores: [ 9.5, 14, 13, 13, 16.5, 16.5 ],
+          points: 82.5,
+          worst: 22.5
         },
         {
-          rank: 10,
-          number: 991,
-          driver: 'Felipe Hernandez',
-          scores: [ 0, 0, 0, 0, 22.5, 1 ],
-          worst: 0,
+          rank: 11,
+          number: 108,
+          driver: 'Fabian Vargas',
+          team: 'valerio',
           country: 'CR',
-          team: 'advanced'
+          scores: [ 9, 18.5, 7.5, 15, 9, 11 ],
+          points: 70,
+          worst: 16.5
         },
         {
-          rank: 10,
-          number: 917,
-          driver: 'Julian Gutierrez',
-          scores: [ 13, 0, 0, 0, 0, 0 ],
-          worst: 0,
+          rank: 12,
+          number: 197,
+          driver: 'Valentina Giraldo',
+          team: 'fik',
           country: 'CR',
-          team: 'fsa'
+          scores: [ 20.5, 0, 0, 0, 11, 9 ],
+          points: 40.5,
+          worst: 0
+        },
+        {
+          rank: 13,
+          number: 144,
+          driver: 'Abraham Murillo',
+          team: 'Advanced',
+          country: 'CR',
+          scores: [ 32, 0, 0, 0, 0, 0 ],
+          points: 32,
+          worst: 0
+        },
+        {
+          rank: 14,
+          number: 172,
+          driver: 'Luca de la Cruz',
+          team: 'Advanced',
+          country: 'CR',
+          scores: [ 10, 13.5, 6.5, 0, 0, 0 ],
+          points: 30,
+          worst: 0
+        },
+        {
+          rank: 15,
+          number: 164,
+          driver: 'Julian de la Cruz',
+          team: 'Advanced',
+          country: 'CR',
+          scores: [ 8.5, 5, 9, 0, 0, 0 ],
+          points: 22.5,
+          worst: 0
         }
       ]
     }
