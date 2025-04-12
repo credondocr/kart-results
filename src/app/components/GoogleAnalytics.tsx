fae238d44c512e40553afcabae1d00933da866cc
Cesar Redondo <credondocr@gmail.com>
2025-04-11 21:10:56 -0600

    Add google analytics

--- src/app/components/GoogleAnalytics.tsx
+++ src/app/components/GoogleAnalytics.tsx
@@ -5,19 +5,19 @@ export default function GoogleAnalytics() {
         <>
             <Script
                 strategy="afterInteractive"
-                src="https://www.googletagmanager.com/gtag/js?id=G-LPK6W05FXS
+                src="https://www.googletagmanager.com/gtag/js?id=G-LPK6W05FXS"
             />
             <Script
                 id="google-analytics"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-                __html: `
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'G-LPK6W05FXS');
           `,
-            }}
+                }}
             />
         </>
     )
